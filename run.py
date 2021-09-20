e2896a9a9f1e903caec4ad1d53e41b61fabbf157
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-20 15:28:14 +0900

    asdf

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-import torch_baselines.DQN.dqn
-
+from torch_baselines.DQN.dqn import DQN 
+from mlagents_envs.environment import UnityEnvironment,ActionTuple
+from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
+from mlagents_envs.side_channel.environment_parameters_channel import EnvironmentParametersChannel
 
