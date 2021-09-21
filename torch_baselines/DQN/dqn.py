9abfe54a3378284d380ee7e7208ea81b6f69aff2
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-21 17:59:51 +0900

    asdf

--- torch_baselines/DQN/dqn.py
+++ torch_baselines/DQN/dqn.py
@@ -116,6 +116,7 @@ class DQN:
         nxtobses = [torch.from_numpy(o).float() for o in data[3]]
         nxtobses = [no.permute(0,3,1,2) if len(no.shape) == 4 else no for no in nxtobses]
         dones = (~torch.from_numpy(data[4])).float()
+        self.model.eval()
         vals = self.model(obses).gather(-1,actions.view(-1,1))
         with torch.no_grad():
             next_vals = dones*torch.max(self.target_model(nxtobses),1)[0]
