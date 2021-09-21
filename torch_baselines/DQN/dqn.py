90826d6127c395c3613b36dee472673ca8654189
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-21 20:37:25 +0900

    asdf

--- torch_baselines/DQN/dqn.py
+++ torch_baselines/DQN/dqn.py
@@ -13,7 +13,7 @@ import torch
 class DQN:
     def __init__(self, policy, env, gamma=0.99, learning_rate=1e-3, buffer_size=50000, exploration_fraction=0.1,
                  exploration_final_eps=0.02, exploration_initial_eps=1.0, train_freq=1, batch_size=32, double_q=True,
-                 learning_starts=1000, target_network_update_freq=10000, prioritized_replay=False,
+                 learning_starts=1000, target_network_update_freq=500, prioritized_replay=False,
                  prioritized_replay_alpha=0.6, prioritized_replay_beta0=0.4, prioritized_replay_beta_iters=None,
                  prioritized_replay_eps=1e-6, param_noise=False, verbose=0, tensorboard_log=None, 
                  _init_setup_model=True, policy_kwargs=None, full_tensorboard_log=False, seed=None):
