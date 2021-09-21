c86d5140c423b6869f53f08f3d2682938da5d744
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-21 17:17:29 +0900

    asdf

--- torch_baselines/DQN/dqn.py
+++ torch_baselines/DQN/dqn.py
@@ -118,7 +118,7 @@ class DQN:
         dones = (~torch.from_numpy(data[4])).float()
         vals = self.model(obses).gather(-1,actions.view(-1,1))
         with torch.no_grad():
-            next_vals = dones*torch.max(self.target_model(nxtobses),dim=-1)[0]
+            next_vals = dones*torch.max(self.target_model(nxtobses),dim=-1)
             targets = rewards + self.gamma*next_vals
         loss = torch.mean(torch.square(targets - vals))
         m_targets = torch.mean(targets)
