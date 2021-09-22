2b17f5ba549719dd05d0b091ec267762fa3a0e1d
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-23 01:47:08 +0900

    asdf

--- torch_baselines/DQN/dqn.py
+++ torch_baselines/DQN/dqn.py
@@ -190,6 +190,8 @@ class DQN:
         
     def learn(self, total_timesteps, callback=None, log_interval=100, tb_log_name="DQN",
               reset_num_timesteps=True, replay_wrapper=None):
+        if self.dualing_model:
+            tb_log_name = "Dualing_" + tb_log_name
         if self.double_q:
             tb_log_name = "Double_" + tb_log_name
         if self.prioritized_replay:
