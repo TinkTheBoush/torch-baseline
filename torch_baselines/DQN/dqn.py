6abb8f51b51b273c699615d47e060a1b9e1564f3
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-22 15:44:20 +0900

    asdf

--- torch_baselines/DQN/dqn.py
+++ torch_baselines/DQN/dqn.py
@@ -121,7 +121,7 @@ class DQN:
         if self.prioritized_replay:
             def weighted_mse_loss(input, target, weight):
                 td_errors = input - target
-                return torch.mean(weight * td_errors ** 2), td_errors.squeeze().detach().numpy()
+                return (weight * td_errors ** 2).mean(), td_errors.squeeze().detach().numpy()
                 #return torch.mean(td_errors ** 2), td_errors.squeeze().detach().numpy()
             self.loss = weighted_mse_loss
         else:
