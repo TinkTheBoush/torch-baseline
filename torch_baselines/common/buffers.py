9175549ee1de3a36de97b07d9106527260220e4e
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-23 18:47:13 +0900

    asdf

--- torch_baselines/common/buffers.py
+++ torch_baselines/common/buffers.py
@@ -161,8 +161,8 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         """
         idx = self._next_idx
         super().add(obs_t, action, reward, obs_tp1, done)
-        self._it_sum[idx] = self._max_priority ** self._alpha
-        self._it_min[idx] = self._max_priority ** self._alpha
+        self._it_sum[idx] = self._max_priority
+        self._it_min[idx] = self._max_priority
 
     def extend(self, obs_t, action, reward, obs_tp1, done):
         """
--- torch_baselines/common/losses.py
+++ torch_baselines/common/losses.py
@@ -22,7 +22,26 @@ class WeightedHuber(_Loss):
         self.beta = beta
 
     def forward(self, input: Tensor, target: Tensor, weight : Tensor) -> Tensor:
-        return (weight * F.smooth_l1_loss(input, target, reduction='none', beta=self.beta).squeeze()).mean(-1) 
+        return (weight * F.smooth_l1_loss(input, target, reduction='none', beta=self.beta).squeeze()).mean(-1)
+    
+class Categorial51Loss(_Loss):
+    __constants__ = ['reduction']
+    def __init__(self, size_average=None, reduce=None, reduction: str = 'mean', categorial_bar = None) -> None:
+        super(Categorial51Loss, self).__init__(size_average, reduce, reduction)
+        self.categorial_bar = categorial_bar
+        self.bar_start = categorial_bar[:-1]
+        self.bar_end = categorial_bar[1:]
+
+    def forward(self, input_distribution: Tensor, next_distribution: Tensor, next_categorial_bar: Tensor) -> Tensor:
+        with torch.no_grad():
+            Tz = next_categorial_bar.clamp(self.categorial_bar[0], self.categorial_bar[-1])
+            C51_b = (Tz - C51_vmin) / C51_delta
+            C51_L = C51_b.floor().to(torch.int64)
+            C51_U = C51_b.ceil().to(torch.int64)
+            C51_L[ (C51_U > 0)               * (C51_L == C51_U)] -= 1
+            C51_U[ (C51_L < (C51_atoms - 1)) * (C51_L == C51_U)] += 1
+            target_distribution = next_distribution
+        return F.binary_cross_entropy_with_logits(input_distribution,target_distribution)
 
 def categorial_loss(input_distribution,next_distribution,categorial_bar,next_categorial_bar):
     bar_start = categorial_bar[:-1]
