419c42fd6d627b2c5e9bf0a1e52e967c245cff84
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-23 17:43:04 +0900

    asdf

--- torch_baselines/DQN/dqn.py
+++ torch_baselines/DQN/dqn.py
@@ -173,9 +173,6 @@ class DQN:
             loss = self.loss(vals,targets,weights)
         else:
             loss = self.loss(vals,targets)
-            
-        print(loss)
-        #loss = self.loss(vals,targets)
         
         self.optimizer.zero_grad()
         loss.backward()
