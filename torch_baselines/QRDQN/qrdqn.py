cb8a1270f70ef216d18d3be165ecf6f2f6ece552
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-24 17:14:05 +0900

    asdf

--- torch_baselines/QRDQN/qrdqn.py
+++ torch_baselines/QRDQN/qrdqn.py
@@ -162,6 +162,8 @@ class QRDQN:
         nxtobses = [no.permute(0,3,1,2) if len(no.shape) == 4 else no for no in nxtobses]
         dones = (~torch.from_numpy(data[4]).to(self.device)).float().view(-1,1,1)
         self.model.train()
+        mm = self.model(obses)
+        print(mm)
         vals = self.model(obses).gather(1,actions)
         print(vals.shape)
         with torch.no_grad():
@@ -169,6 +171,8 @@ class QRDQN:
                 action = self.model(nxtobses).mean(2).max(1)[1].view(-1,1,1)
             else:
                 action = self.target_model(nxtobses).mean(2).max(1)[1].view(-1,1,1)
+            tt = self.target_model(nxtobses)
+            print(tt)
             next_vals = dones*self.target_model(nxtobses).gather(1,action)
             print(next_vals.shape)
             targets = (next_vals * self.gamma) + rewards
