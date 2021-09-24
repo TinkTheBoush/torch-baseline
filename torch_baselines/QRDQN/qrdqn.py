cc4d3f2989635c10f2b57a44f4696c357b768004
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-24 17:04:14 +0900

    asdf

--- torch_baselines/QRDQN/qrdqn.py
+++ torch_baselines/QRDQN/qrdqn.py
@@ -163,14 +163,16 @@ class QRDQN:
         dones = (~torch.from_numpy(data[4]).to(self.device)).float().view(-1,1,1)
         self.model.train()
         vals = self.model(obses).gather(1,actions)
+        print(vals.shape)
         with torch.no_grad():
             if self.double_q:
                 action = self.model(nxtobses).mean(2).max(1)[1].view(-1,1,1)
             else:
                 action = self.target_model(nxtobses).mean(2).max(1)[1].view(-1,1,1)
             next_vals = dones*self.target_model(nxtobses).gather(1,action)
+            print(next_vals.shape)
             targets = (next_vals * self.gamma) + rewards
-        print(targets.shape)
+        
         logit_valid_tile = targets.view(-1,self.n_support,1)
         theta_loss_tile = vals.view(-1,1,self.n_support)
         
