e2fb11360c249748da6e295143d72040ba257d5d
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 02:54:56 +0900

    asdf

--- torch_baselines/common/losses.py
+++ torch_baselines/common/losses.py
@@ -51,6 +51,8 @@ class CategorialDistributionLoss(_Loss):
             C51_U[ (C51_L < (self.categorial_bar_n - 1)) * (C51_L == C51_U)] += 1
             self.offset = self.offset.to(next_distribution).int()
             target_distribution = input_distribution.new_zeros(self.batch_size, self.categorial_bar_n) # Returns a Tensor of size size filled with 0. same dtype
+            print((C51_U + self.offset).view(-1).max()[0])
+            print(target_distribution.view(-1).shape())
             target_distribution.view(-1).index_add_(0, (C51_L + self.offset).view(-1), (next_distribution * (C51_U.float() - C51_b)).view(-1))
             target_distribution.view(-1).index_add_(0, (C51_U + self.offset).view(-1), (next_distribution * (C51_b - C51_L.float())).view(-1))
         return F.binary_cross_entropy_with_logits(input_distribution,target_distribution, reduction='none')
