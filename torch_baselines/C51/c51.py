76dd81dd549a2f86391124832892b21a4ccad0e0
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 04:49:54 +0900

    asdf

--- torch_baselines/C51/c51.py
+++ torch_baselines/C51/c51.py
@@ -80,6 +80,7 @@ class C51(Q_Network_Family):
                 next_actions = (self.target_model(nxtobses)*self._categorial_bar).mean(2).max(1)[1].view(-1,1,1).repeat_interleave(self.categorial_bar_n, dim=2)
             next_distribution = self.target_model(nxtobses).gather(1,next_actions).squeeze()
             targets_categorial_bar = (dones * self.categorial_bar * self._gamma) + rewards
+            print(targets_categorial_bar)
             
         if self.prioritized_replay:
             weights = torch.from_numpy(data[5]).to(self.device)
