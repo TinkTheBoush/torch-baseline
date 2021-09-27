b253248f52d0f974a5fc9cecec7a571031e66472
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 05:31:39 +0900

    asdfasdf

--- torch_baselines/common/losses.py
+++ torch_baselines/common/losses.py
@@ -46,13 +46,13 @@ class CategorialDistributionLoss(_Loss):
             C51_b = (Tz - self.min) / self.delta
             C51_L = C51_b.floor().int()
             C51_U = C51_b.ceil().int()
-            #C51_L[ (C51_U > 0)               * (C51_L == C51_U)] -= 1
-            #C51_U[ (C51_L < (self.categorial_bar_n - 1)) * (C51_L == C51_U)] += 1
+            C51_L[ (C51_U > 0)               * (C51_L == C51_U)] -= 1
+            C51_U[ (C51_L < (self.categorial_bar_n - 1)) * (C51_L == C51_U)] += 1
             self.offset = self.offset.to(next_distribution).int()
             target_distribution = input_distribution.new_zeros(self.batch_size, self.categorial_bar_n) # Returns a Tensor of size size filled with 0. same dtype
             target_distribution.view(-1).index_add_(0, (C51_L + self.offset).view(-1), (next_distribution * (C51_U.float() - C51_b)).view(-1))
             target_distribution.view(-1).index_add_(0, (C51_U + self.offset).view(-1), (next_distribution * (C51_b - C51_L.float())).view(-1))
-        return -(target_distribution * input_distribution.log()).sum(-1)
+        return F.binary_cross_entropy_with_logits(input_distribution,target_distribution, reduction='none').sum(-1)
         #F.binary_cross_entropy_with_logits(input_distribution,target_distribution, reduction='none').sum(-1)
         #-(target_distribution * input_distribution.log()).sum(-1)
 
