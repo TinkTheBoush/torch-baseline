ae4933d8985557e2f10d7192762cab2321e016da
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 02:36:31 +0900

    asdf

--- torch_baselines/common/losses.py
+++ torch_baselines/common/losses.py
@@ -53,8 +53,8 @@ class CategorialDistributionLoss(_Loss):
             target_distribution = input_distribution.new_zeros(self.batch_size, self.categorial_bar_n) # Returns a Tensor of size size filled with 0. same dtype
             target_distribution.view(-1).index_add_(0, (C51_L + self.offset).view(-1).to(torch.int64), (next_distribution * (C51_U.float() - C51_b)).view(-1))
             target_distribution.view(-1).index_add_(0, (C51_U + self.offset).view(-1).to(torch.int64), (next_distribution * (C51_b - C51_L.float())).view(-1))
-            #target_distribution = next_distribution
-        return F.binary_cross_entropy_with_logits(input_distribution,target_distribution, reduction='none')
+        return -(target_distribution * input_distribution.log()).sum(-1)
+        #F.binary_cross_entropy_with_logits(input_distribution,target_distribution, reduction='none')
 '''
 def project_distribution(batch_state, batch_action, non_final_next_states, batch_reward, non_final_mask):
     """
