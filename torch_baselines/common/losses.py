deedd6478cba739144f3840a773c0d9714917dd7
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 05:08:29 +0900

    asdf

--- torch_baselines/common/losses.py
+++ torch_baselines/common/losses.py
@@ -46,8 +46,8 @@ class CategorialDistributionLoss(_Loss):
             C51_b = (Tz - self.min) / self.delta
             C51_L = C51_b.floor().int()
             C51_U = C51_b.ceil().int()
-            C51_L[ (C51_U > 0)               * (C51_L == C51_U)] -= 1
-            C51_U[ (C51_L < (self.categorial_bar_n - 1)) * (C51_L == C51_U)] += 1
+            #C51_L[ (C51_U > 0)               * (C51_L == C51_U)] -= 1
+            #C51_U[ (C51_L < (self.categorial_bar_n - 1)) * (C51_L == C51_U)] += 1
             self.offset = self.offset.to(next_distribution).int()
             target_distribution = input_distribution.new_zeros(self.batch_size, self.categorial_bar_n) # Returns a Tensor of size size filled with 0. same dtype
             target_distribution.view(-1).index_add_(0, (C51_L + self.offset).view(-1), (next_distribution * (C51_U.float() - C51_b)).view(-1))
