9fa41be72bff0342f873a7f48756c681c363d727
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 17:20:05 +0900

    asdf

--- torch_baselines/C51/c51.py
+++ torch_baselines/C51/c51.py
@@ -85,8 +85,6 @@ class C51(Q_Network_Family):
                 logsum = torch.logsumexp((sum_q - sum_q.max(1)[0].unsqueeze(-1))/self.munchausen_entropy_tau , 1).unsqueeze(-1)
                 tau_log_pi_next = (sum_q - sum_q.max(1)[0].unsqueeze(-1) - self.munchausen_entropy_tau*logsum).unsqueeze(-1)
                 pi_target = torch.nn.functional.softmax(sum_q/self.munchausen_entropy_tau, dim=1).unsqueeze(-1)
-                print(tau_log_pi_next.shape)
-                print(pi_target.shape)
                 categorial_bar = self.categorial_bar - (pi_target*tau_log_pi_next).sum(1)
                 
                 q_k_targets = (self.target_model(obses)*self._categorial_bar).sum(2)
