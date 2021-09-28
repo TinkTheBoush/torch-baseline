d126be28bc3f2f3deda23c07b2f40649c4147a4f
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 10:27:01 +0900

    asdf

--- torch_baselines/QRDQN/qrdqn.py
+++ torch_baselines/QRDQN/qrdqn.py
@@ -78,7 +78,7 @@ class QRDQN(Q_Network_Family):
             weights = torch.from_numpy(data[5]).to(self.device)
             indexs = data[6]
             losses = self.loss(theta_loss_tile,logit_valid_tile,self.quantile)
-            new_priorities = losses.detach().cpu().clone().numpy() + self.prioritized_replay_eps
+            new_priorities = np.max(np.nan_to_num(losses.detach().cpu().clone().numpy()) + self.prioritized_replay_eps,self.prioritized_replay_eps)
             self.replay_buffer.update_priorities(indexs,new_priorities)
             loss = losses.mean(-1)
             loss = (weights*losses).mean(-1)
