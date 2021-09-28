51055b8075e277723c8b9c9e0378670fc88c4341
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 15:47:03 +0900

    asdf

--- run.py
+++ run.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     parser.add_argument('--per', action='store_true')
     parser.add_argument('--noisynet', action='store_true')
     parser.add_argument('--n_step', type=int, default=1, help='n step setting when n > 1 is n step td method')
+    parser.add_argument('--munchausen', action='store_true')
     parser.add_argument('--steps', type=float, default=1e6, help='step size')
     parser.add_argument('--verbose', type=int, default=0, help='verbose')
     parser.add_argument('--logdir',type=str, default='log/',help='log file dir')
@@ -53,17 +54,17 @@ if __name__ == "__main__":
     if args.algo == "DQN":
         agent = DQN(env,batch_size = args.batch, target_network_update_freq = args.target_update,
                     prioritized_replay = args.per, double_q = args.double, dualing_model = args.dualing,
-                    param_noise = args.noisynet, n_step = args.n_step,
+                    param_noise = args.noisynet, n_step = args.n_step, munchausen = args.munchausen,
                     tensorboard_log=args.logdir+env_name)
     elif args.algo == "C51":
         agent = C51(env,batch_size = args.batch, target_network_update_freq = args.target_update,
                     prioritized_replay = args.per, double_q = args.double, dualing_model = args.dualing,
-                    param_noise = args.noisynet, n_step = args.n_step,
+                    param_noise = args.noisynet, n_step = args.n_step, munchausen = args.munchausen,
                     tensorboard_log=args.logdir+env_name)
     elif args.algo == "QRDQN":
         agent = QRDQN(env,batch_size = args.batch, target_network_update_freq = args.target_update,
                     prioritized_replay = args.per, double_q = args.double, dualing_model = args.dualing,
-                    param_noise = args.noisynet, n_step = args.n_step,
+                    param_noise = args.noisynet, n_step = args.n_step, munchausen = args.munchausen,
                     tensorboard_log=args.logdir+env_name)
 
     agent.learn(int(args.steps))
