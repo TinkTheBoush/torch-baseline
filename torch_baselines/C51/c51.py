50016919c2f47003034dff7ec5f0783b18a5d1e8
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-09-28 17:07:10 +0900

    asdf

--- torch_baselines/C51/c51.py
+++ torch_baselines/C51/c51.py
@@ -10,14 +10,14 @@ class C51(Q_Network_Family):
                  exploration_final_eps=0.02, exploration_initial_eps=1.0, train_freq=1, batch_size=32, double_q=True,
                  dualing_model = False, n_step = 1, learning_starts=1000, target_network_update_freq=2000, prioritized_replay=False,
                  prioritized_replay_alpha=0.6, prioritized_replay_beta0=0.4, prioritized_replay_eps=1e-6, 
-                 param_noise=False, verbose=0, tensorboard_log=None, _init_setup_model=True, policy_kwargs=None, 
+                 param_noise=False, munchausen=False, verbose=0, tensorboard_log=None, _init_setup_model=True, policy_kwargs=None, 
                  full_tensorboard_log=False, seed=None):
         
         super(C51, self).__init__(env, gamma, learning_rate, buffer_size, exploration_fraction,
                  exploration_final_eps, exploration_initial_eps, train_freq, batch_size, double_q,
                  dualing_model, n_step, learning_starts, target_network_update_freq, prioritized_replay,
                  prioritized_replay_alpha, prioritized_replay_beta0, prioritized_replay_eps, 
-                 param_noise, verbose, tensorboard_log, _init_setup_model, policy_kwargs, 
+                 param_noise, munchausen, verbose, tensorboard_log, _init_setup_model, policy_kwargs, 
                  full_tensorboard_log, seed)
         
         self.categorial_bar_n = categorial_bar_n
