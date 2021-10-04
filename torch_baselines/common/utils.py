9dcd88e9838387e9713ac73d311d4e1ece20b969
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-04 17:39:21 +0900

    asdf

--- torch_baselines/common/utils.py
+++ torch_baselines/common/utils.py
@@ -11,4 +11,5 @@ def get_flatten_size(function,size):
     return function(torch.rand(*([1]+size))).data.shape[-1]
 
 def convert_states(obs : List, device : torch.device):
-    return [torch.tensor(o,dtype=torch.float32,device=device).permute(0,3,1,2) if len(o.shape) == 4 else torch.tensor(o,dtype=torch.float32,device=device) for o in obs]
+    return [torch.tensor(o,dtype=torch.float32,device=device).permute(0,3,1,2) 
+                if len(o.shape) == 4 else torch.tensor(o,dtype=torch.float32,device=device) for o in obs]
