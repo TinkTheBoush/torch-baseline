a3b9ae1ada21b472ac6cd1670da46ce78e286034
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-05 23:27:03 +0900

    asdasdfasdf

--- torch_baselines/TD3/td3.py
+++ torch_baselines/TD3/td3.py
@@ -81,8 +81,9 @@ class TD3(Deterministic_Policy_Gradient_Family):
         with torch.no_grad():
             next_actions = self.target_actor(nxtobses)
             next_actions = torch.clamp(next_actions + 
-                                       torch.clamp(torch.normal(0,self.target_action_noise,size=(self.batch_size, self.action_size[0])),
-                                                   -self.action_noise_clamp,self.action_noise_clamp),
+                                       torch.clamp(
+                                           torch.normal(0,self.target_action_noise,size=(self.batch_size, self.action_size[0]),device=self.device),
+                                            -self.action_noise_clamp,self.action_noise_clamp),
                                        -1,1)
             next_vals1, next_vals2 = self.target_critic(nxtobses,next_actions)
             next_vals = dones * torch.minimum(next_vals1, next_vals2)
