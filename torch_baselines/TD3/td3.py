a721c130527a10e6b184b1f09c52f259674b29ec
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-06 23:37:36 +0900

    asdfasdfasdf

--- torch_baselines/TD3/td3.py
+++ torch_baselines/TD3/td3.py
@@ -29,8 +29,9 @@ class TD3(Deterministic_Policy_Gradient_Family):
             
     def actions(self,obs,befor_train):
         if not befor_train:
-            actions = np.clip(self.actor(convert_states(obs,self.device)).detach().cpu().clone().numpy() + 
-                              np.random.normal(0,self.action_noise,size=(self.worker_size,self.action_size[0])),-1,1)
+            with torch.no_grad():
+                actions = np.clip(self.actor(convert_states(obs,self.device)).detach().cpu().clone().numpy() + 
+                                np.random.normal(0,self.action_noise,size=(self.worker_size,self.action_size[0])),-1,1)
         else:
             actions = np.clip(np.random.normal(0,self.action_noise,size=(self.worker_size,self.action_size[0])),-1,1)
         return actions
@@ -102,7 +103,7 @@ class TD3(Deterministic_Policy_Gradient_Family):
             critic_loss2 = self.critic_loss(vals2,targets).mean(-1)
         critic_loss = critic_loss1 + critic_loss2
          
-        self.critic_optimizer.zero_grad()
+        self.critic_optimizer.zero_grad(set_to_none=True)
         critic_loss.backward()
         self.critic_optimizer.step()
         
@@ -110,7 +111,7 @@ class TD3(Deterministic_Policy_Gradient_Family):
             q1,_ = self.critic(obses,self.actor(obses))
             actor_loss = -q1.squeeze().mean(-1)
             
-            self.actor_optimizer.zero_grad()
+            self.actor_optimizer.zero_grad(set_to_none=True)
             actor_loss.backward()
             torch.nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
             self.actor_optimizer.step()
