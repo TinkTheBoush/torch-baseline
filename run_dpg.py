d10de879cfd71eeefc623c1207479fb34c6c6599
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-07 23:02:07 +0900

    asdfasdfasdf

--- run_dpg.py
+++ run_dpg.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         channel = EnvironmentParametersChannel()
         
         env = UnityEnvironment(file_name=env_name,no_graphics=True, side_channels=[engine_configuration_channel,channel],timeout_wait=10000)
-        engine_configuration_channel.set_configuration_parameters(time_scale=20.0)
+        engine_configuration_channel.set_configuration_parameters(time_scale=8.0)
         env_name = env_name.split('/')[-1].split('.')[0]
         
     else:
