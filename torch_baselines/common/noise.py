44a90376c6cf5f14a7012dd90de9d27568019123
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-07 16:57:18 +0900

    asdasdfasdf

--- torch_baselines/common/noise.py
+++ torch_baselines/common/noise.py
@@ -8,7 +8,7 @@ class OUNoise(object):
         self.noise_prev = None
         self.action_size = action_size
         self.worker_size = worker_size
-        self.noise_prev = self._sigma * np.random.normal(size=(self.worker_size,self.action_size))
+        self.noise_prev = np.random.normal(0,self._sigma,size=(self.worker_size,self.action_size))
     
     def __call__(self) -> np.ndarray:
         noise = self.noise_prev - self._theta * self.noise_prev + \
