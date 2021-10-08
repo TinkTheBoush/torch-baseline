6efec341c26bdc93a4ecc5b034b14af9b61d18f1
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-08 20:49:09 +0900

    comment

--- torch_baselines/DDPG/ddpg.py
+++ torch_baselines/DDPG/ddpg.py
@@ -167,8 +167,8 @@ class DDPG(Deterministic_Policy_Gradient_Family):
                     self.summary.add_scalar("episode_reward", self.scores[id], steps)
                 self.scores[id] = 0
             for id in dec.agent_id:
-                if (id in old_term_id and id not in old_dec_id) or id in term.agent_id:
-                    continue
+                if (id in old_term_id and id not in old_dec_id) or id in term.agent_id: 
+                    continue #if in old_term_id -> start dec but not in old_dec_id -> second dec, if in term.agent_id -> start dec
                 obs = old_dec[id].obs
                 nxtobs = dec[id].obs
                 reward = dec[id].reward
