ad884ed1e3afd447e27373f1a55fa56ef1db34d2
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-14 03:37:59 +0900

    asdfasdfasdfasdf

--- torch_baselines/common/utils.py
+++ torch_baselines/common/utils.py
@@ -17,9 +17,9 @@ def set_random_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
 
-def convert_states(obs : List, device : torch.device):
-    return [torch.as_tensor(o,dtype=torch.float32,device=device).permute(0,3,1,2) 
-                if len(o.shape) == 4 else torch.as_tensor(o,dtype=torch.float32,device=device) for o in obs]
+def convert_states(obs : List, device : torch.device, dtype=torch.float):
+    return [torch.as_tensor(o,dtype=dtype,device=device).permute(0,3,1,2) 
+                if len(o.shape) == 4 else torch.as_tensor(o,dtype=dtype,device=device) for o in obs]
     #return [torch.tensor(o,dtype=torch.float32,device=device).permute(0,3,1,2) 
     #            if len(o.shape) == 4 else torch.tensor(o,dtype=torch.float32,device=device) for o in obs]
 
