cd8079cab6e1deb007ff7e8d1f47c1f2c39d3599
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-14 22:02:13 +0900

    asdfasdfasdfasdf

--- run_dpg.py
+++ run_dpg.py
@@ -36,6 +36,7 @@ if __name__ == "__main__":
     parser.add_argument('--node', type=int,default=256, help='network node number')
     parser.add_argument('--hidden_n', type=int,default=2, help='hidden layer number')
     parser.add_argument('--action_noise', type=float,default=0.1, help='action noise std')
+    parser.add_argument('--target_noise_mul', type=float,default=1.5, help='target noise mul')
     args = parser.parse_args() 
     env_name = args.env
     env_type = ""
@@ -64,18 +65,21 @@ if __name__ == "__main__":
     elif args.algo == "TD3":
         agent = TD3(env,batch_size = args.batch, learning_rate=args.lr, gamma = args.gamma, train_freq=args.train_freq, 
                     gradient_steps=args.grad_step, buffer_size= int(args.buffer_size), target_network_tau= args.target_update_tau,
-                    prioritized_replay = args.per, action_noise = args.action_noise, n_step = args.n_step, max_grad_norm = args.max_grad,
+                    prioritized_replay = args.per, target_action_noise_mul= args.target_noise_mul ,action_noise = args.action_noise,
+                    n_step = args.n_step, max_grad_norm = args.max_grad,
                     tensorboard_log=args.logdir + env_type + "/" +env_name, policy_kwargs=policy_kwargs)
     elif args.algo == "TD4_QR":
         agent = TD4_QR(env,batch_size = args.batch, learning_rate=args.lr, gamma = args.gamma, train_freq=args.train_freq, 
                        gradient_steps=args.grad_step, buffer_size= int(args.buffer_size), target_network_tau= args.target_update_tau,
-                    prioritized_replay = args.per, action_noise = args.action_noise, n_step = args.n_step, max_grad_norm = args.max_grad, 
+                    prioritized_replay = args.per, target_action_noise_mul= args.target_noise_mul , action_noise = args.action_noise, 
+                    n_step = args.n_step, max_grad_norm = args.max_grad, 
                     risk_avoidance = args.risk_avoidance, n_support=args.n_support,
                     tensorboard_log=args.logdir + env_type + "/" +env_name, policy_kwargs=policy_kwargs)
     elif args.algo == "TD4_IQN":
         agent = TD4_IQN(env,batch_size = args.batch, learning_rate=args.lr, gamma = args.gamma, train_freq=args.train_freq, 
                        gradient_steps=args.grad_step, buffer_size= int(args.buffer_size), target_network_tau= args.target_update_tau,
-                    prioritized_replay = args.per, action_noise = args.action_noise, n_step = args.n_step, max_grad_norm = args.max_grad, 
+                    prioritized_replay = args.per, target_action_noise_mul= args.target_noise_mul , action_noise = args.action_noise, 
+                    n_step = args.n_step, max_grad_norm = args.max_grad, 
                     risk_avoidance = args.risk_avoidance, n_support=args.n_support,
                     tensorboard_log=args.logdir + env_type + "/" +env_name, policy_kwargs=policy_kwargs)
 
