78d3bab2d4a990968bb1abf9375f3d8fb2d14fc8
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-14 16:03:12 +0900

    asdfasdfasdfasdf

--- torch_baselines/DDPG/base_class.py
+++ torch_baselines/DDPG/base_class.py
@@ -145,6 +145,7 @@ class Deterministic_Policy_Gradient_Family(object):
                 self.scoreque.append(score)
                 if self.summary:
                     self.summary.add_scalar("episode_reward", score, steps)
+                    self.summary.add_scalar("done",float(done),steps)
                 self.scores[id] = 0
             for id in dec.agent_id:
                 if id in old_term_id or id in term.agent_id:
@@ -189,6 +190,7 @@ class Deterministic_Policy_Gradient_Family(object):
                 self.scoreque.append(self.scores[0])
                 if self.summary:
                     self.summary.add_scalar("episode_reward", self.scores[0], steps)
+                    self.summary.add_scalar("done",float(done),steps)
                 self.scores[0] = 0
                 state = self.env.reset()
                 
