dbd66d2ca37f2ae286e65aabef8b9a1b71c8c92f
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-18 16:12:25 +0900

    asdfasdfasdf

--- torch_baselines/SAC/sac.py
+++ torch_baselines/SAC/sac.py
@@ -24,13 +24,6 @@ class SAC(Deterministic_Policy_Gradient_Family):
         if _init_setup_model:
             self.setup_model()
             
-    def actions(self,obs,befor_train):
-        if not befor_train:
-            with torch.no_grad():
-                actions = np.clip(self.actor(convert_states(obs,self.device)).detach().cpu().clone().numpy(),-1,1)
-        else:
-            actions = np.clip(np.random.normal(0,self.action_noise,size=(self.worker_size,self.action_size[0])),-1,1)
-        return actions
             
     def setup_model(self):
         self.policy_kwargs = {} if self.policy_kwargs is None else self.policy_kwargs
@@ -120,6 +113,14 @@ class SAC(Deterministic_Policy_Gradient_Family):
             self.summary.add_scalar("loss/targets", targets.mean(), steps)
 
         return critic_loss.detach().cpu().clone().numpy()
+    
+    def actions(self,obs,befor_train):
+        if not befor_train:
+            with torch.no_grad():
+                actions = np.clip(self.actor(convert_states(obs,self.device)).detach().cpu().clone().numpy(),-1,1)
+        else:
+            actions = np.random.uniform(-1,1,size=(self.worker_size,self.action_size[0]))
+        return actions
     
     def learn(self, total_timesteps, callback=None, log_interval=1000, tb_log_name="SAC",
               reset_num_timesteps=True, replay_wrapper=None):
