02ce5a513795ab5682a3514b10614fde159940ea
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-10-18 16:24:33 +0900

    asdfasdfasdf

--- torch_baselines/SAC/sac.py
+++ torch_baselines/SAC/sac.py
@@ -4,7 +4,7 @@ import numpy as np
 from torch_baselines.DDPG.base_class import Deterministic_Policy_Gradient_Family
 from torch_baselines.SAC.network import Actor, Critic
 from torch_baselines.common.losses import MSELosses, HuberLosses
-from torch_baselines.common.utils import convert_states, hard_update, soft_update
+from torch_baselines.common.utils import convert_tensor, hard_update, soft_update
 
 class SAC(Deterministic_Policy_Gradient_Family):
     def __init__(self, env, gamma=0.99, learning_rate=5e-4, buffer_size=50000, train_freq=1, gradient_steps=1,
@@ -62,10 +62,10 @@ class SAC(Deterministic_Policy_Gradient_Family):
             data = self.replay_buffer.sample(self.batch_size)
 
         with torch.no_grad():
-            obses = convert_states(data[0],self.device)
+            obses = convert_tensor(data[0],self.device)
             actions = torch.tensor(data[1],dtype=torch.float32,device=self.device)
             rewards = torch.tensor(data[2],dtype=torch.float32,device=self.device).view(-1,1)
-            nxtobses = convert_states(data[3],self.device)
+            nxtobses = convert_tensor(data[3],self.device)
             dones = (~torch.tensor(data[4],dtype=torch.bool,device=self.device)).float().view(-1,1)
             q1_pi, q2_pi, _ = self.critic(obses,self.actor(obses))
             _, _, target_vals = self.target_critic(nxtobses,actions)
@@ -118,7 +118,7 @@ class SAC(Deterministic_Policy_Gradient_Family):
     def actions(self,obs,befor_train):
         if not befor_train:
             with torch.no_grad():
-                actions = np.clip(self.actor(convert_states(obs,self.device)).detach().cpu().clone().numpy(),-1,1)
+                actions = np.clip(self.actor(convert_tensor(obs,self.device)).detach().cpu().clone().numpy(),-1,1)
         else:
             actions = np.random.uniform(-1,1,size=(self.worker_size,self.action_size[0]))
         return actions
